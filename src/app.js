e13e686b9a062f73c716d638380ecbe883bad415
Filipe Câncio <filipe.cancio@outlook.com>
2020-08-09 23:17:15 -0300

    first change

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -11,23 +12,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title , url , techs } = request.body;
+  const project = {id:uuid(),title , url , techs, likes: 0};
+  repositories.push(project);
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex(i =>  i.id == id );
+
+  if(index<0){
+    response.status(400);
+  }
+  
+  let { title , url , techs } = request.body;
+  repositories[index].title = title ? title: repositories[index].title;
+  repositories[index].url = url ? url: repositories[index].url;
+  repositories[index].techs = techs ? techs: repositories[index].techs;
+
+
+  return response.json({ title , url , techs });
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex(i =>  i.id == id );
+
+  if(index<0){
+    response.status(400);
+  }
+
+  const project = repositories.splice(index,1);
+  return response.status(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex(i =>  i.id == id );
+
+  if(index<0){
+    response.status(400);
+  }
+
+  repositories[index].likes += 1;
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
