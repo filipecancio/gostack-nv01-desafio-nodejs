81368a010e8659219cbd8ceac328ecc4285276b2
Filipe Câncio <filipe.cancio@outlook.com>
2020-08-10 00:29:38 -0300

    FInish to fix all tests

--- src/app.js
+++ src/app.js
@@ -33,13 +33,13 @@ app.put("/repositories/:id", (request, response) => {
     return response.status(400).send();
   }
   
-  let { title , url , techs } = request.body;
+  let { title , url , techs, likes } = request.body;
   repositories[index].title = title ? title: repositories[index].title;
   repositories[index].url = url ? url: repositories[index].url;
   repositories[index].techs = techs ? techs: repositories[index].techs;
+  likes = repositories[index].likes;
 
-
-  return response.json({ title , url , techs });
+  return response.json({id, title , url , techs, likes });
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -71,7 +71,7 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(400).send();
   }
 
-  repositories[index].likes += 1;
+  repositories[index].likes ++;
   return response.json(repositories[index]);
 });
 
